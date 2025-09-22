37cdd60c022346a3b93c6ab0d3a0685e1ffd1eec
Syed Ashik Mahmud Pullock <pullock15-4981@diu.edu.bd>
2025-09-22 21:52:18 +0600

    add it in development

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <div className="min-h-screen bg-background text-foreground flex justify-center items-center text-5xl font-bold">
       হে শুরু করেন........
       <div className="text-2xl font-medium mt-4">
-        আপনার যাত্রা শুরু হচ্ছে এখানে।
+        আপনার যাত্রা শুরু হচ্ছে এখানে।......
       </div>
     </div>
   );
