ee9f7058ef5f8586bf4e56f094e45a1c572008aa
tauhid <tauhidbd789@gmail.com>
2025-09-23 11:38:37 +0600

    structure change

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+// app/layout.tsx
+
+import { ThemeProvider } from '@/components/ui/ThemeProvider';
+import { Geist, Geist_Mono } from 'next/font/google';
+
+import './globals.css';
+
+const geistSans = Geist({ subsets: ['latin'], variable: '--font-geist', display: 'swap' });
+const geistMono = Geist_Mono({
+  subsets: ['latin'],
+  variable: '--font-geist-mono',
+  display: 'swap',
+});
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en" className="antialiased" suppressHydrationWarning>
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <AuthProvider>
+          <ThemeProvider>{children}</ThemeProvider>
+        </AuthProvider>
+      </body>
+    </html>
+  );
+}
