6f25c394fd3e0cbb28dcb9915d36cfd3c7234763
Syed Ashik Mahmud Pullock <143836708+Pullock4981@users.noreply.github.com>
2025-10-07 16:17:03 +0600

    Update Navbar.tsx

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -9,6 +9,7 @@ import {
   User,
   Settings,
   LogOut,
+  LayoutDashboard,
 } from "lucide-react";
 import { useRef, useState } from "react";
 import { motion } from "framer-motion";
@@ -126,9 +127,10 @@ const Navbar = () => {
                           <User className="w-4 h-4" />
                           Profile
                         </Link>
-                        <Link href={"/dashboard/settings"} className="w-full flex items-center gap-3 px-3 py-2 text-sm hover:bg-secondary rounded-md transition-colors">
-                          <Settings className="w-4 h-4" />
-                          Settings
+                        <Link href={"/dashboard"} className="w-full flex items-center gap-3 px-3 py-2 text-sm hover:bg-secondary rounded-md transition-colors">
+                          
+                          <LayoutDashboard className="w-4 h-4"  />
+                          Dashboard
                         </Link>
                         <div className="border-t border-border my-1" />
                         <button
