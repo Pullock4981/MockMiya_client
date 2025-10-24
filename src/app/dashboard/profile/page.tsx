40fc2298d9564a4a586e6337643cbe1178779343
Mahir Siyam <mahirmubtasimsiyam@gmail.com>
2025-10-24 21:42:56 +0600

    solved deploy issue

--- src/app/dashboard/profile/page.tsx
+++ src/app/dashboard/profile/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import React, { useState, useEffect } from 'react';
-import { useAuth } from '@/context/AuthContext';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
