4c58445e17444d6f155dc5fa1c9334bfcd56ecb0
Tauhid <tauhidbd789@gmail.com>
2025-10-28 12:25:48 +0600

    error fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,43 @@
+// 'use client';
+
+// import { ThemeProvider } from '@/components/ui/ThemeProvider';
+// import { Geist, Geist_Mono } from 'next/font/google';
+// import { SessionProvider } from 'next-auth/react';
+// import { AuthProvider } from '@/context/AuthContext/AuthContext';
+// import type { ReactNode } from 'react';
+
+// import './globals.css';
+
+// const geistSans = Geist({
+//   subsets: ['latin'],
+//   variable: '--font-geist',
+//   display: 'swap',
+// });
+
+// const geistMono = Geist_Mono({
+//   subsets: ['latin'],
+//   variable: '--font-geist-mono',
+//   display: 'swap',
+// });
+
+// interface RootLayoutProps {
+//   readonly children: ReactNode;
+// }
+
+// export default function RootLayout({ children }: RootLayoutProps) {
+//   return (
+//     <html lang="en" className="antialiased" suppressHydrationWarning>
+//       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+//         <SessionProvider>
+//           <AuthProvider>
+//             <ThemeProvider>{children}</ThemeProvider>
+//           </AuthProvider>
+//         </SessionProvider>
+//       </body>
+//     </html>
+//   );
+// }
+
 "use client";
 
 import { ThemeProvider } from "@/components/ui/ThemeProvider";
