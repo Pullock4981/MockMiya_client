1b40d19ea0de7069d2bdb6c6850410bfc6c53b84
Jahangir-alam25 <jahangiralam10022003@gmail.com>
2025-10-29 18:19:14 +0600

    changed Job Analysis API key

--- src/app/api/job-analyzer/route.ts
+++ src/app/api/job-analyzer/route.ts
@@ -39,7 +39,7 @@ export async function POST(req: NextRequest) {
 
     // Step 2: Send to Gemini
     const geminiResponse = await fetch(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${process.env.GOOGLE_API_KEY}`,
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${process.env.JOB_ANALYTICS_API_KEY}`,
       {
         method: "POST",
         headers: { "Content-Type": "application/json" },
